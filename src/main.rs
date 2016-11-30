cdd0808705ccfe21b34b1d2453e0603afc451696
SethDusek <shibe@openmailbox.org>
2016-11-30 23:07:14 +0500

    Remove threading for now

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ use getopts::{Matches, Options};
 
 static KEY: &'static str = "a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
 static UKEY: &'static str = "ub2JDDg9Iumsh1HfdO3a3HQbZi0up1qe8LkjsnWQvyVvQLFn1q";
-const THREAD_ENABLED: bool = false;
+//const THREAD_ENABLED: bool = true;
 struct Config {
     max_definitions: i16,
 }
@@ -103,7 +103,6 @@ fn get_source<'a, T: Dictionary + ?Sized, K: Thesaurus + ?Sized>
 }
 
 
-
 fn main() {
     let (mut dictionaries, mut thesaureses) = get_sources();
     let (opts, args) = parse_args(); //we needs opts too for printing brief description
@@ -111,66 +110,12 @@ fn main() {
     if args.free.is_empty() {
         println!("{}", opts.usage("USAGE: define WORD"));
     }
-    if ! THREAD_ENABLED {
-        for word in &args.free {
-            println!("{}:", word.to_uppercase());
-            print_definition(dictionary, &word.to_lowercase(), Some(3)).unwrap_or_else(|err| println!("{}", err));
-            if args.opt_present("t") {
-                println!("SYNONYMS:");
-                print_synonyms(thesaurus, word).unwrap_or_else(|err| println!("{}", err));
-            }
+    for word in &args.free {
+        println!("{}:", word.to_uppercase());
+        print_definition(dictionary, &word.to_lowercase(), Some(3)).unwrap_or_else(|err| println!("{}", err));
+        if args.opt_present("t") {
+            println!("SYNONYMS:");
+            print_synonyms(thesaurus, word).unwrap_or_else(|err| println!("{}", err));
         }
-    }
-    else {
-        let free_clone = args.free.clone();
-        let dict_arc = Arc::new(dictionary.clone_to_box());
-        //chunks.map(|chunk| chunks_vec.push(chunk));
-        let (tx, rx) = mpsc::channel::<Vec<Definition>>();
-        let mut words: Vec<&[String]> = Vec::new();
-        let threads = num_cpus::get();;
-        {
-            let threads = num_cpus::get();
-            for chunk in free_clone.chunks((free_clone.len() + threads - 1) / threads) {
-                words.push(chunk);
-            }
-        }
-        let words_arc = Arc::new(words);
-        for num in 0..words_arc.len() {
-            let dict = dict_arc.clone();
-            let tx = tx.clone();
-            let words = words_arc.clone()[num];
-            unsafe {
-            crossbeam::spawn_unsafe(move || {
-                let mut dictionary = dict.clone_to_box();
-                let mut definitions = Vec::new();
-                for word in words.iter() {
-                    let defs = dictionary.get_definitions(word).unwrap_or(Vec::new());
-                    for definition in &defs {
-                        definitions.push(definition.clone());
-                    }
-                }
-                tx.send(definitions);
-            });
-            }
-        }   
-        let mut definitions: HashMap<String, Vec<Definition>> = HashMap::new();
-        for _ in 0..words_arc.len() { 
-            let thread_definitions = rx.recv().unwrap();
-            for definition in thread_definitions {
-                if definitions.contains_key(&definition.word) {
-                    definitions.get_mut(&definition.word).unwrap().push(definition);
-                }
-
-                else {
-                    definitions.insert(definition.word.clone().to_lowercase(), vec![definition]); //lowercase is necessary because some dictionaries return the definitions in different cases
-                }
-            }
-        }
-        for (word, definitions) in definitions.iter() {
-                println!("{}:", word.to_uppercase());
-                for (number, definition) in definitions.iter().take(3).enumerate() {
-                    println!("{}. {}", number + 1, definition.text);
-                }
-            }
     }
 }

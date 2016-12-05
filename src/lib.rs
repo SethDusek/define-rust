18c4998a0f836477cf09d877357ee73e2912d78d
SethDusek <shibe@openmailbox.org>
2016-12-05 19:40:15 +0500

    Switch to serde_derive

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
-#![feature(plugin, custom_derive)]
+#![feature(proc_macro, custom_derive, custom_attribute)]
 #![feature(box_syntax)]
-#![plugin(serde_macros)]
-extern crate serde;
+#[macro_use] extern crate serde_derive;
 pub mod dictionaries;
 pub mod thesaureses;

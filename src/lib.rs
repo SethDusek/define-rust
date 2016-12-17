5d871f052075bcb810d8d0da6787de1fdd8bffe4
SethDusek <shibe@openmailbox.org>
2016-12-17 23:51:17 +0500

    Add from implementations

--- src/lib.rs
+++ src/lib.rs
@@ -9,18 +9,13 @@ pub use dictionaries::{Definition, Dictionary};
 pub use thesaureses::Thesaurus;
 mod error {
     use std::error;
+    use std::error::Error as ErrorT;
     use std::fmt;
     #[derive(Debug)]
     pub enum Error {
         Hyper(::hyper::Error),
         Serde(::serde_json::Error),
         Error(Box<error::Error>)
-    }
-    impl fmt::Display for Error {
-        fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-            fmt.write_str(self.description())?;
-            Ok(())
-        }
     }
     impl error::Error for Error {
         fn description(&self) -> &str {
@@ -32,7 +27,23 @@ mod error {
         }
         fn cause(&self) -> Option<&::std::error::Error> { None }
     }
+    impl fmt::Display for Error {
+        fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+            fmt.write_str(self.description())?;
+            Ok(())
+        }
+    }
+    impl From<::hyper::Error> for Error {
+        fn from(val: ::hyper::Error) -> Self {
+            Error::Hyper(val)
+        }
+    }
+    impl From<::serde_json::Error> for Error {
+        fn from(val: ::serde_json::Error) -> Self {
+            Error::Serde(val)
+        }
+    }
 }
         
 pub use error::Error;
-
+pub type Result<T> = std::result::Result<T, Error>;
